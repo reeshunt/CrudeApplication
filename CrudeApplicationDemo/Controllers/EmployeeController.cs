f93aa0fab9002bf0657ace80f98406360ee190b7
Admin <Admin@LAPTOP-DE0M0TJ5>
2021-05-29 16:39:32 +0530

    Applied server side validation

--- CrudeApplicationDemo/Controllers/EmployeeController.cs
+++ CrudeApplicationDemo/Controllers/EmployeeController.cs
@@ -15,16 +15,23 @@ namespace CrudeApplicationDemo.Controllers
         }
         public JsonResult AddEmployee(Tbl_Emp dataObj)
         {
-            try
+            if (ModelState.IsValid)
             {
-                UspRoughEntities1 dbContext = new UspRoughEntities1();
-                dbContext.Usp_Emp(dataObj.EmployeeName,dataObj.EmpId,dataObj.Designation,dataObj.DateOfBirth,dataObj.DateOfRetirement,dataObj.ContactNumber,dataObj.Address,dataObj.FileUpload,dataObj.PhotoUpload);
-                dbContext.SaveChanges();
-                return Json(new { msg = "success" }, JsonRequestBehavior.AllowGet);
+                try
+                {
+                    UspRoughEntities1 dbContext = new UspRoughEntities1();
+                    dbContext.Usp_Emp(dataObj.EmployeeName, dataObj.EmpId, dataObj.Designation, dataObj.DateOfBirth, dataObj.DateOfRetirement, dataObj.ContactNumber, dataObj.Address, dataObj.FileUpload, dataObj.PhotoUpload);
+                    dbContext.SaveChanges();
+                    return Json(new { msg = "success" }, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception Ex)
+                {
+                    return Json(new { msg = "error", errorMsg = Ex.Message }, JsonRequestBehavior.AllowGet);
+                }
             }
-            catch(Exception Ex)
+            else
             {
-                return Json(new { msg = "error" ,errorMsg=Ex.Message}, JsonRequestBehavior.AllowGet);
+                return Json(new { msg = "error", errorMsg = "Enter Valid Details" }, JsonRequestBehavior.AllowGet);
             }
             
         }
---
+++ CrudeApplicationDemo/Models/Validation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CrudeApplicationDemo.Models
+{
+    public  class Validation
+    {
+        public string EmpId { get; set; }
+        [Required]
+        public string EmployeeName { get; set; }
+        [Required(ErrorMessage = "Enter Designation")]
+        public string Designation { get; set; }
+
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
+        public Nullable<System.DateTime> DateOfRetirement { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "Enter Valid PhoneNumber")]
+        public string ContactNumber { get; set; }
+        public string Address { get; set; }
+
+        [RegularExpression(@"^.*\.(pdf|PDF)$", ErrorMessage = "File is not valid")]
+        public string FileUpload { get; set; }
+        [RegularExpression(@"^.*\.(jpg|JPG|jpeg|JPEG|)$", ErrorMessage = "Photo is not valid")]
+        public string PhotoUpload { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+    }
+}
