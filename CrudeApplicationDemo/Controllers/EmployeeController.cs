e2d352c66bef13bdb325af3bc58486d1ed124813
Kumar Rishabh <krishabh619@planetecomsolutions.com>
2021-05-29 13:47:26 +0530

    Form made

---
+++ CrudeApplicationDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudeApplicationDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employe
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
