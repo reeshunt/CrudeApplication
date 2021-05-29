08577a860d498f461a508d78d4756187c9156681
Admin <Admin@LAPTOP-DE0M0TJ5>
2021-05-29 15:33:31 +0530

    Saved data in DB

--- CrudeApplicationDemo/Controllers/EmployeeController.cs
+++ CrudeApplicationDemo/Controllers/EmployeeController.cs
@@ -13,6 +13,21 @@ namespace CrudeApplicationDemo.Controllers
         {
             return View();
         }
+        public JsonResult AddEmployee(Tbl_Emp dataObj)
+        {
+            try
+            {
+                UspRoughEntities1 dbContext = new UspRoughEntities1();
+                dbContext.Usp_Emp(dataObj.EmployeeName,dataObj.EmpId,dataObj.Designation,dataObj.DateOfBirth,dataObj.DateOfRetirement,dataObj.ContactNumber,dataObj.Address,dataObj.FileUpload,dataObj.PhotoUpload);
+                dbContext.SaveChanges();
+                return Json(new { msg = "success" }, JsonRequestBehavior.AllowGet);
+            }
+            catch(Exception Ex)
+            {
+                return Json(new { msg = "error" ,errorMsg=Ex.Message}, JsonRequestBehavior.AllowGet);
+            }
+            
+        }
         
         public ActionResult EmpDetails()
         {
